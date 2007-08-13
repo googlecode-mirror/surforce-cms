de4932082bf2a5205ebe59d7d88736e4bbd130fe
enriqueplace <enriqueplace@81a1fa64-3c34-0410-90ac-bb6ae85f8f3a>
2007-08-13 00:09:02 +0000

    Primer prototipo estable

---
+++ tags/1.0.0/application/controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+class IndexController extends Zend_Controller_Action{
+
+	function init(){
+		$this->initView();
+		$this->view->baseUrl = $this->_request->getBaseUrl();
+		$this->view->user = Zend_Auth::getInstance()->getIdentity();
+	}
+
+	function indexAction(){
+		$this->view->title = "CMS - Home";
+		$this->render();
+	}
+
+}
+
+?>
