2852e12f560b7536b8709ecf80548d4b22d63ad1
andresguzf <andresguzf@gmail.com>
2007-10-05 23:57:02 +0000

    Cambios FCKeditor

---
+++ public/scripts/fckeditor/myconfig.js
@@ -0,0 +1,14 @@
+﻿FCKConfig.AutoDetectLanguage = false ;
+/*FCKConfig.DefaultLanguage = "es" ;*/
+FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/default/' ;
+FCKConfig.UseBROnCarriageReturn	= true ;	// IE only.
+/*FCKConfig.FormatSource		= true ;*/
+
+FCKConfig.LinkBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Connector=connectors/php/connector.php';
+FCKConfig.ImageBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Image&Connector=connectors/php/connector.php';
+FCKConfig.FlashBrowserURL = FCKConfig.BasePath + 'filemanager/browser/default/browser.html?Type=Flash&Connector=connectors/php/connector.php';
+
+FCKConfig.ImageUpload = true ;
+FCKConfig.ImageUploadURL = FCKConfig.BasePath + 'filemanager/upload/php/upload.php?Type=Image' ;
+FCKConfig.ImageUploadAllowedExtensions	= ".(jpg|gif|jpeg|png)$" ;		// empty for all
+FCKConfig.ImageUploadDeniedExtensions	= "" ;
