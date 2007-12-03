03295829e11aee9e46c3d2d57764ec5f4165c66e
jrviana <jrviana@81a1fa64-3c34-0410-90ac-bb6ae85f8f3a>
2007-12-03 05:46:13 +0000

    Se agregan menus dinamicos

---
+++ trunk/sql/4_menus_menus_items.v2.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `menus_items` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `id_menu` int(10) unsigned NOT NULL,
+  `item` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `destino` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `posicion` tinyint(2) unsigned NOT NULL,
+  `privado` char(1) collate utf8_unicode_ci NOT NULL,
+  `estado` char(1) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+INSERT INTO `menus_items` (`id`, `id_menu`, `item`, `destino`, `posicion`, `privado`, `estado`) VALUES
+(1, 1, 'Menus', '/menus/menus', 1, '0', '1'),
+(2, 2, 'Google', 'google.com', 1, '0', '1'),
+(3, 1, 'Usuarios', '/usuarios/usuarios', 2, '1', '1');
