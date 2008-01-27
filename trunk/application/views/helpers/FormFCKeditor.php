1d4dbe1a46d1d6c70f4ee58e13f64a51b71ed628
enriqueplace <enriqueplace@81a1fa64-3c34-0410-90ac-bb6ae85f8f3a>
2008-01-27 23:43:52 +0000

    Ajustes estéticos en las vistas de Noticias

--- trunk/application/views/helpers/FormFCKeditor.php
+++ trunk/application/views/helpers/FormFCKeditor.php
@@ -7,20 +7,20 @@ class Zend_View_Helper_FormFCKeditor
    private $_sBasePath;
    private $_oFCKeditor;
 
-	public function formFCKeditor($instanceName = null, $contentValue = null )
+	public function formFCKeditor($instanceName = null, $contentValue = null, $width = '100%', $height = '600' )
 	{
-   	$this->_instanceName = $instanceName ;
-      $this->_contentValue = $contentValue;
-      $sBasePath = $_SERVER['PHP_SELF'];
-      $sBasePath = substr( $sBasePath, 0, strpos( $sBasePath, "index.php" ) );
+   		$this->_instanceName = $instanceName ;
+      	$this->_contentValue = $contentValue;
+      	$sBasePath = $_SERVER['PHP_SELF'];
+      	$sBasePath = substr( $sBasePath, 0, strpos( $sBasePath, "index.php" ) );
 
-      $this->_sBasePath = $sBasePath . "public/scripts/fckeditor/";
-      $this->_oFCKeditor = new FCKeditor($this->_instanceName) ;
-      $this->_oFCKeditor->BasePath    = $this->_sBasePath ;
-      $this->_oFCKeditor->Value        =  $this->_contentValue;
-      $this->_oFCKeditor->Config['CustomConfigurationsPath'] = '../myconfig.js' ;
-		$this->_oFCKeditor->Width  = '100%' ;
-		$this->_oFCKeditor->Height = '600' ;
+      	$this->_sBasePath = $sBasePath . "public/scripts/fckeditor/";
+      	$this->_oFCKeditor = new FCKeditor($this->_instanceName) ;
+      	$this->_oFCKeditor->BasePath    = $this->_sBasePath ;
+      	$this->_oFCKeditor->Value        =  $this->_contentValue;
+      	$this->_oFCKeditor->Config['CustomConfigurationsPath'] = '../myconfig.js' ;
+		$this->_oFCKeditor->Width  = $width;
+		$this->_oFCKeditor->Height = $height;
 		return $this->_oFCKeditor->Create();
      }
  }
