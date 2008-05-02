2999368762404b19a27e7e205864e73d55eb638e
enriqueplace <enriqueplace@81a1fa64-3c34-0410-90ac-bb6ae85f8f3a>
2008-05-02 18:52:22 +0000

    - bug: error en método de helper; funciona en Win pero no en Lin ;-) - indentación

--- trunk/library/Zcms/Generic/Controller.php
+++ trunk/library/Zcms/Generic/Controller.php
@@ -1,26 +1,28 @@
 <?php
 
-abstract class Zcms_Generic_Controller extends Zend_Controller_Action 
+abstract class Zcms_Generic_Controller extends Zend_Controller_Action
 {
  	protected $registry = null;
     protected $session = null;
 	protected $debug = null;
-	
-	public function init() 
+
+	public function init()
 	{
 		parent::init();
 		$this->registry = Zend_Registry::getInstance();
-		        
+
 		$this->initView ();
 
-		
 		$this->view->setScriptPath( './application/views/scripts/' );
+
 		$this->view->setHelperPath( './application/views/helpers/', 'Helper' );
 		$this->view->addHelperPath( './library/Zcms/View/helper/', 'Zcms_View_Helper' );
-        $this->view->addHelperPath('./library/Zsurforce/View/Helper/', 'Zsurforce_View_Helper');   		
+        $this->view->addHelperPath('./library/Zsurforce/View/Helper/', 'Zsurforce_View_Helper');
+
         $this->view->addBasePath('./public/','');
+
 		$this->view->baseUrl = $this->_request->getBaseUrl ();
-		        
+
 		Zend_Loader::loadClass ( 'Configuracion' );
 		$this->view->user = Zend_Auth::getInstance ()->getIdentity ();
 		$this->info = $this->registry->get('personalizacion');
@@ -28,26 +30,26 @@ abstract class Zcms_Generic_Controller extends Zend_Controller_Action
 
 		$this->session = $this->registry->get('session');
         $this->view->session = $this->session;
-        
+
         $this->debug = $this->registry->get('debug');
         $this->view->debug = $this->debug;
-        
+
 		/* información de sitios y subsitios */
 		$this->view->sitios = Sitios::getAll()->toArray();
 		$this->view->configuracion = Configuracion::getConfiguracion($this->session->sitio->id);
-		
+
 		$this->registrarSitio();
-		$this->cargarMenuHorizontal();   
-		
+		$this->cargarMenuHorizontal();
+
 	}
 	protected function registrarSitio()
 	{
 		$sitio = $this->_request->getParam ('sitio', NULL);
-		
-		if(!$sitio OR $sitio == "default"){			
+
+		if(!$sitio OR $sitio == "default"){
 			if(!$this->session->sitio){
-				$this->session->sitio = Sitios::getSitioDefault();			
-			}										
+				$this->session->sitio = Sitios::getSitioDefault();
+			}
 		}else{
 			$this->session->sitio = Sitios::getSitio($sitio);
 		}
@@ -56,19 +58,19 @@ abstract class Zcms_Generic_Controller extends Zend_Controller_Action
 	protected function cargarMenuHorizontal()
 	{
 		$opciones = array();
-		
+
 		/*
     	 * FIXME: eliminar los tildes para las urls ( url => nombre)
     	 */
-		
+
 		foreach( Sitios::getAll()->toArray() as $op ){
 			$opciones[] = array(
 				'url' => '/default/index/index/sitio/'.strtolower($op['nombre']),
 				'alt' =>  strtolower($op['nombre']),
 				'text' => ucfirst($op['titulo'])
 			);
-		}		
-		$this->view->menuHorizontalOpciones = $opciones;		
+		}
+		$this->view->menuHorizontalOpciones = $opciones;
 	}
 }
 ?>
