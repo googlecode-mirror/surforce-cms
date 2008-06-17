e8d79aad2a98f0213fe57f1553671b617435a8db
enriqueplace <enriqueplace@81a1fa64-3c34-0410-90ac-bb6ae85f8f3a>
2008-06-17 15:18:23 +0000

    Se elimina copia de Zsurforce del proyecto CMS y se actualizó la librería del proyecto.
    CMS debe bajarla del proyecto Library y no tener una copia distinta de forma local.

--- library/Zsurforce/ZGdata/Gmail/EmptyArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'ZsurForce/ZGdata/Gmail/Exception.php';
-
-class ZsurForce_ZGdata_Gmail_EmptyArgumentException extends ZsurForce_ZGdata_Gmail_Exception
-{
-}
--- library/Zsurforce/ZGdata/Gmail/Exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'ZsurForce/Exception.php';
- 
-class ZsurForce_ZGdata_Gmail_Exception extends Zsurforce_Exception
-{
-}
-?>
