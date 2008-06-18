bc6fdf9bbf462f3989d1b3b6cbc4e429891f3e75
enriqueplace <enriqueplace@81a1fa64-3c34-0410-90ac-bb6ae85f8f3a>
2008-06-18 06:41:42 +0000

    Se eliminan todos los helpers que son genéricos y se agregan en surforce-library.
    De ahora en más debería CMS usar Library para acceder a estos helpers

--- trunk/application/views/helpers/HtmpLinkOrder.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Helper_HtmlLinkOrder
-{
-	public function htmlLinkOrder( $url, $orden, $asc, $text )
-	{
-		$xhtml = "<a href=".$url.'/orden/'.$orden.'/asc/'.!$asc.">".$text."</a>";
-		return $xhtml;
-	}
-}
--- trunk/application/views/helpers/PaginaTitulo.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Helper_PaginaTitulo
-{
-	public function paginaTitulo( $text )
-	{
-		return "<h2>" . $text . "</h2>";
-	}
-}
