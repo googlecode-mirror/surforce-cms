f3c422dfc9da04a19ad44c60ce92d90e05a6ef9a
enriqueplace <enriqueplace@81a1fa64-3c34-0410-90ac-bb6ae85f8f3a>
2008-07-20 20:19:13 +0000

    Bug:
    - se mantienen los datos del form de contacto, antes cuando se daba un campo vacío, al recargar, se perdían los datos ya ingresados

--- trunk/application/modules/frontend/controllers/ContactoController.php
+++ trunk/application/modules/frontend/controllers/ContactoController.php
@@ -15,34 +15,41 @@ class Frontend_ContactoController extends Zcms_Generic_Controller
     function agregarAction()
     {
         $this->view->subtitle = $this->info->sitio->contacto->agregar->titulo;
-
+        $this->view->contacto = new stdClass();
+        $this->view->action = $this->info->sitio->usuarios->agregar->action;
+        $this->view->buttonText = $this->info->sitio->contacto->agregar->buttonText;
+            
         if ($this->_request->isPost()) {
             Zend_Loader::loadClass('Zend_Filter_StripTags');
             $filter 	= new Zend_Filter_StripTags();
             
-            $nombre 	= trim($filter->filter($this->_request->getPost('nombre')));
-            $email  	= trim($filter->filter($this->_request->getPost('email')));
-            $comentario = trim($filter->filter($this->_request->getPost('comentario')));
-            $telefono   = trim($filter->filter($this->_request->getPost('telefono')));
+            $this->view->contacto->nombre 	= trim($filter->filter($this->_request->getPost('nombre')));
+            $this->view->contacto->email  	= trim($filter->filter($this->_request->getPost('email')));
+            $this->view->contacto->comentario = trim($filter->filter($this->_request->getPost('comentario')));
+            $this->view->contacto->telefono   = trim($filter->filter($this->_request->getPost('telefono')));
             $fecha		= date("Y-m-d H:i:s");
             
-            if ($nombre != '' && $email != '' && $comentario != '' && $telefono !='' ) {
+            if ($this->view->contacto->nombre != '' && 
+                $this->view->contacto->email != '' && 
+                $this->view->contacto->comentario != '' && 
+                $this->view->contacto->telefono !='' ) {
+                	
                 $data = array(
-                    'nombre' 	=> $nombre,
-                    'email' 	=> $email,
-                    'comentario'=> $comentario,
-                    'telefono'  => $telefono,
+                    'nombre' 	=> $this->view->contacto->nombre,
+                    'email' 	=> $this->view->contacto->email,
+                    'comentario'=> $this->view->contacto->comentario,
+                    'telefono'  => $this->view->contacto->telefono,
                     'fecha' 	=> $fecha,
                 	'id_sitio'	=> $this->session->sitio->id
                 );
                 $contacto = new Contacto();
                 $contacto->insert($data);
+                
                 //Enviamos el correo.
-                $destinatario = $email;
-                $asunto = $this->info->sitio->contacto->add->asunto;
-                //$mensaje = $this->info->sitio->contacto->add->mensaje;
-                $cuerpo = "$nombre"." "."$comentario";
-                $headers = $this->info->sitio->contacto->add->sender;;
+                $destinatario   = $this->view->contacto->email;
+                $asunto         = $this->info->sitio->contacto->add->asunto;
+                $cuerpo         = $this->view->contacto->nombre." ".$this->view->contacto->comentario;
+                $headers        = $this->info->sitio->contacto->add->sender;;
                 
                 mail($destinatario,$asunto,$cuerpo,$headers);
 
@@ -53,14 +60,6 @@ class Frontend_ContactoController extends Zcms_Generic_Controller
                 $this->view->message = "Deben completar todos los campos";
             }
         }
-
-        $this->view->contacto = new stdClass();
-        $this->view->contacto->id = null;
-        $this->view->contacto->nombre = '';
-        $this->view->contacto->email = '';
-        $this->view->contacto->comentario = '';
-        $this->view->action = $this->info->sitio->usuarios->agregar->action;
-        $this->view->buttonText = $this->info->sitio->contacto->agregar->buttonText;
         $this->render();
     }
 }
